1d04535a93ba262fa443b2cb99479d6efa72788b
ilushegg <marnopolskiyi@mail.ru>
2022-07-14 10:24:58 +0300

    Added the ability to upload files to the server and connected styles to the project

--- src/main/java/com/example/Sweater/domain/Message.java
+++ src/main/java/com/example/Sweater/domain/Message.java
@@ -14,7 +14,7 @@ public class Message {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User author;
-
+    private String filename;
     public String getAuthorName(){
         return author != null ? author.getUsername() : "<none>";
     }
@@ -57,5 +57,13 @@ public class Message {
 
     public void setAuthor(User author) {
         this.author = author;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
     }
 }
--- src/main/resources/templates/parts/common.ftlh
+++ src/main/resources/templates/parts/common.ftlh
@@ -3,6 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <title>Spring Security Example </title>
+    <link rel="stylesheet" href="/static/style.css">
 </head>
 <body>
 <#nested>
