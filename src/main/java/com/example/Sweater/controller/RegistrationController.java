d720539892798ac41199077d818069486a668e30
ilushegg <marnopolskiyi@mail.ru>
2022-07-14 13:07:26 +0300

    Added email service to a project, the user's email has been verified

--- src/main/java/com/example/Sweater/controller/RegistrationController.java
+++ src/main/java/com/example/Sweater/controller/RegistrationController.java
@@ -1,38 +1,48 @@
 package com.example.Sweater.controller;
 
-import com.example.Sweater.domain.Role;
 import com.example.Sweater.domain.User;
-import com.example.Sweater.repos.UserRepo;
+import com.example.Sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
     @Autowired
-    private UserRepo userRepo;
+    private UserService userService;
 
     @GetMapping("/registration")
     public String registration(){
+        System.out.println("ehehgergsdg");
+
         return "registration";
     }
 
     @PostMapping("/registration")
     public String addUser(User user, Map<String, Object> model){
-        User userFromDB = userRepo.findByUsername(user.getUsername());
-
-        if(userFromDB != null){
+        if(!userService.addUser(user)){
+            System.out.println(user.getEmail());
             model.put("message", "User Exists!");
             return "registration";
         }
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepo.save(user);
+
 
         return "redirect:/login";
+    }
+    @GetMapping("/activate/{code}")
+    public String activate(Model model, @PathVariable String code){
+        boolean isActivated = userService.activateUser(code);
+
+        if(isActivated){
+            model.addAttribute("message", "User successfully activated");
+        }else{
+            model.addAttribute("message", "Activation code is not found!");
+        }
+        return "login";
     }
 }
