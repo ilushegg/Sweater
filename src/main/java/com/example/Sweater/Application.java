b8f8d46434473ab333d63d09f13d75517cb139a2
ilushegg <marnopolskiyi@mail.ru>
2022-07-15 11:07:50 +0300

    Added flyway dependency to migrate databases into the project. Made some migrates

--- src/main/java/com/example/Sweater/Application.java
+++ src/main/java/com/example/Sweater/Application.java
@@ -3,10 +3,22 @@ package com.example.Sweater;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+    @Bean
+    public FlywayMigrationStrategy cleanMigrateStrategy() {
+        return flyway -> {
+            flyway.repair();
+            flyway.migrate();
+        };
+    }
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
+
+
     }
+
 }
