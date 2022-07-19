eea02203f58d623c2597781d4deb9f9aee7469bb
ilushegg <marnopolskiyi@mail.ru>
2022-07-19 07:51:22 +0300

    Made tests

---
+++ src/test/java/com/example/Sweater/LoginTest.java
@@ -0,0 +1,50 @@
+package com.example.Sweater;
+
+
+import com.example.Sweater.controller.MainController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LoginTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private MainController controller;
+
+    @Test
+    public void contextLoads() throws Exception{
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, guest")))
+                .andExpect(content().string(containsString("Please, login")));
+    }
+
+    @Test
+    public void accessDeniedTest() throws Exception{
+        this.mockMvc.perform(get("/main"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login"));
+    }
+    @Test
+    public void correctLoginTest() throws Exception{
+        this.mockMvc.perform(formLogin().user("1234").password("2012"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    }
+}
