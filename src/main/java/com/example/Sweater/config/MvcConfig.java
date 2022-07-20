47e29a140e6d8ca45d4a14c4de5f81eaf91e27db
ilushegg <marnopolskiyi@mail.ru>
2022-07-20 10:18:24 +0300

    Fixed minor bugs after connection Turbolinks

--- src/main/java/com/example/Sweater/config/MvcConfig.java
+++ src/main/java/com/example/Sweater/config/MvcConfig.java
@@ -1,9 +1,11 @@
 package com.example.Sweater.config;
 
+import com.example.Sweater.domain.util.RedirectInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -26,5 +28,10 @@ public class MvcConfig implements WebMvcConfigurer {
                 .addResourceLocations("file:///" + uploadPath + "/");
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RedirectInterceptor());
     }
 }
